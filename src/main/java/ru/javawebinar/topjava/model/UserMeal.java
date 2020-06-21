127f73b15864c6dd5924f1266d386fe0998751ab
rsaga <connect@me.com>
2020-06-21 11:57:45 +0300

    HW0 do optional2

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,18 +1,24 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 
 public class UserMeal {
+    private static HashMap<LocalDate, Integer> caloriesSummaryPerDay = new HashMap<>();
     private final LocalDateTime dateTime;
-
     private final String description;
-
     private final int calories;
 
     public UserMeal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
+        caloriesSummaryPerDay.put(dateTime.toLocalDate(), caloriesSummaryPerDay.getOrDefault(dateTime.toLocalDate(), 0) + calories);
+    }
+
+    public static int getCaloriesSummaryPerDay(LocalDate day) {
+        return caloriesSummaryPerDay.get(day);
     }
 
     public LocalDateTime getDateTime() {
